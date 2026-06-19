b35820e26c9e46ea946a76572291769a3906b3aa
depderpy <arontang46@gmail.com>
2026-06-19 14:53:48 +0800

    Player_movement

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [SerializeField] private float movementspeed = 0.5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    //The Fixed update is for rendering all the physics 
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveDirection * movementspeed;
+    }
+}
