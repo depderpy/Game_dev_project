e864f56dbf8971aaf7f408634620722faca1e100
depderpy <arontang46@gmail.com>
2026-06-28 01:32:27 +0800

    random encounter line 65

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,9 +6,13 @@ using System.Collections.Generic;
 
 public class player : MonoBehaviour
 {
+
+    [SerializeField] private float encounterNum = 100;
     [SerializeField] private float movementspeed = 5f;
-    [SerializeField] private Tilemap _tiledetector;
-    [SerializeField] private GameObject battletile;
+    [SerializeField] private Tilemap _battletile;
+    [SerializeField] private GameObject _tiledetector;
+
+    private Vector3 _currentTilePosition;
 
     private Rigidbody2D rb;
     private Vector2 moveDirection;
@@ -43,8 +47,23 @@ public class player : MonoBehaviour
     {
         if(!battlezone) return;
 
-        Vector3Int tilepostion = _tiledetector.WorldToCell(_tiledetector.transform.position);
-        TileBase tile = _tiledetector.GetTile(tilepostion);
+
+        //This converts the 3d/world-space Coords to the tilemap coords so the grid's coordinates
+        Vector3Int tilepostion = _battletile.WorldToCell(_tiledetector.transform.position);
+
+        //this assigns the specific battletile sprite on screen to the tile varieble 
+        TileBase tile = _battletile.GetTile(tilepostion);
+
+        //Checking if the player is on the same tile and if they are we won't do more encounter checks cuz that is unfair LOL 
+        if(tile == null || _currentTilePosition == (Vector3) tilepostion) return;
+
+        _currentTilePosition = (Vector3)_currentTilePosition;
+
+        int randomNum = Random.Range(0, 500);
+        if(randomNum < encounterNum)
+        {
+            Debug.Log("Encounter!!");
+        }
     }
 }
     //The Fixed update is for rendering all the physics 
